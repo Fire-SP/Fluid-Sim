1d772b3f7980450bfac9effe3a567623a77cb629
Fire-SP <36900064+Fire-SP@users.noreply.github.com>
2019-03-28 12:02:42 -0600

    Major Fluid Changes
    -Fluid can now flow diagonally.
    -Random chance of fluid moving has been decrease to make fluid act more natural.
    -Added "d" key which allows you to change density of fluid while simulation is running.

--- Main.py
+++ Main.py
@@ -1,63 +1,85 @@
 # air on higher value squares will try to flow to lower value squares
 # a square can only hold a certain amount of air ( Depends on square value ). If square is full, air will not flow into it.
 
-import random, pygame, time, sys
+import random, pygame, time, sys, thorpy
 from noise import pnoise2
+global density
 noise_X = random.randint(-1000,1000)
 noise_Y = random.randint(-1000,1000)
-#Modifiers. Change these to change window size, zoom, and particle size
 noise_zoom = 0.05
-mapsize = 150
-size = 7
+mapsize = 250
+size = 5
+density = 2
 
-##noise_zoom = float(input("Noise? "))
-##mapsize = int(input("Mapsize? "))
-##size = int(input("Paritlcesize? "))
+noise_zoom = float(input("Noise? "))
+mapsize = int(input("Mapsize? "))
+size = int(input("Paritlcesize? "))
+density = int(input("Density? "))
 
 pygame.init()
+info = pygame.display.Info()
 screen = pygame.display.set_mode((mapsize*size,mapsize*size))
 
 air = []
 heightmap = []
 
 def update_air_index(i,j):
-    if i == 0 or i == (mapsize-1) or j == 0 or j == (mapsize-1):
+    if i == 0 or i == (mapsize) or j == 0 or j == (mapsize-1):
         d1 = [255,255,255,255]
         
     else:
-        d1 = [heightmap[i][j-1], heightmap[i][j+1], heightmap[i-1][j], heightmap[i+1][j]]
+        d1 = [heightmap[i][j-1], heightmap[i][j+1], heightmap[i-1][j], heightmap[i+1][j],heightmap[i-1][j-1],heightmap[i+1][j-   1],heightmap[i-1][j+1],heightmap[i+1][j+1],heightmap[i][j]]
 
         target = min(d1)
         target_location = d1.index(min(d1))
-        if target < 14:
-            target_location = random.randint(0,3)
-            
+        if target < 14 and random.randint(0,2) == 0 :
+            target_location = random.randint(0,7)
+        
+        
         if air[i][j] >= 1:
             if heightmap[i][j] >= target:
-                if air[i-1][j] <= 4: 
+                if air[i-1][j] <= density: 
                     if target_location == 2:
                         air[i][j] -= 1
                         air[i-1][j] += 1
 
-                if air[i+1][j] <= 4:
+                if air[i+1][j] <= density:
                     if target_location == 3:
                         air[i][j] -= 1
                         air[i+1][j] += 1
                         
-                if air[i][j-1] <= 4:
+                if air[i][j-1] <= density:
                     if target_location == 0:
                         air[i][j] -= 1
                         air[i][j-1] += 1
 
-                if air[i][j+1] <= 4:
+                if air[i][j+1] <= density:
                     if target_location == 1:
                         air[i][j] -= 1
                         air[i][j+1] += 1
-
-
-            
+                        
+                if air[i-1][j-1] <= density: 
+                    if target_location == 4:
+                        air[i][j] -= 1
+                        air[i-1][j-1] += 1
+                        
+                if air[i+1][j-1] <= density: 
+                    if target_location == 5:
+                        air[i][j] -= 1
+                        air[i+1][j-1] += 1
                 
+                if air[i-1][j+1] <= density: 
+                    if target_location == 6:
+                        air[i][j] -= 1
+                        air[i-1][j+1] += 1
+                        
+                if air[i+1][j+1] <= density: 
+                    if target_location == 7:
+                        air[i][j] -= 1
+                        air[i+1][j+1] += 1
+                        
                 
+      
 def update_air():
     for i in range(mapsize):
         for j in range(mapsize):
@@ -65,21 +87,24 @@ def update_air():
         
 
 def generate_map():
-    for i in range(mapsize):
+    for i in range(mapsize+1):
         air.append([])
         heightmap.append([])
         for j in range(mapsize):
             height = pnoise2((i + (noise_X))*noise_zoom, (j + (noise_Y))*noise_zoom)*10
             height += 15
             heightmap[i].append(abs(height))
-            air[i].append(random.randint(0,5))
+            air[i].append(1)
             
 def key_press(): # Added by u/ delijati. Thank you!
+    global density
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_c and pygame.key.get_mods() and pygame.KMOD_CTRL:
-                print("pressed CTRL-C as an event")
+                print("Quitting with Ctrl-C...")
                 sys.exit(0)
+            if event.key == pygame.K_d:
+                density = int(input("Density? "))
 
             
 def render():
@@ -87,9 +112,11 @@ def render():
         for j in range(mapsize):
             aircolor = air[j][i]*25
             heightcolor = int(heightmap[j][i]*3)
+            if int(heightcolor) > 255:
+                heightcolor = 255
             if aircolor > 255:
                 aircolor=255
-            pygame.draw.circle(screen,(heightcolor,heightcolor,aircolor),(j*size,i*size),size)
+            pygame.draw.circle(screen,(heightcolor*1.2,heightcolor,aircolor),(j*size,i*size),size)
 
             
             
@@ -99,4 +126,3 @@ while True:
     render()
     key_press()
     pygame.display.flip()
-    
